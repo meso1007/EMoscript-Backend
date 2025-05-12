1bb3492f0d6881ccaadab49291a0a309341e0bf3
meso1007 <diegoshoya2017@gmail.com>
2025-05-12 17:27:00 +0900

    add domain

--- backend/settings.py
+++ backend/settings.py
@@ -82,7 +82,10 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://www.emoscript.com",
+    '127.0.0.1', 'localhost'
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
